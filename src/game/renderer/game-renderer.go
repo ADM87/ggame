cfb620e84c711aaf7f9c26291a8f9773d807d35f
Aaron Malley <aaron.d.malley@gmail.com>
2025-06-29 07:58:05 -0300

    background fill color

--- src/game/renderer/game-renderer.go
+++ src/game/renderer/game-renderer.go
@@ -1,26 +1,35 @@
 package renderer
 
 import (
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type Renderer interface {
 	Draw(screen *ebiten.Image)
+	SetClearColor(c color.Color)
 }
 
 type gamerenderer struct {
+	clearColor color.Color
+
 	screenWidth  int
 	screenHeight int
 }
 
 func NewGameRenderer(width, height int) Renderer {
 	return &gamerenderer{
+		clearColor:   color.RGBA{100, 149, 237, 255},
 		screenWidth:  width,
 		screenHeight: height,
 	}
 }
 
 func (g *gamerenderer) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, Ggame!")
+	screen.Fill(g.clearColor)
+}
+
+func (g *gamerenderer) SetClearColor(c color.Color) {
+	g.clearColor = c
 }
